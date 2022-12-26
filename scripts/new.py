8a1cb71b17434d10e0d9289a61f402378dd73cf4
Simon 'Udrian' Haggren <simonhaggren@gmail.com>
2022-12-26 09:32:49 +0100

    Added Template for new file creation when using the new action and added TypeDTest

--- scripts/new.py
+++ scripts/new.py
@@ -37,14 +37,14 @@ def addExtraFiles(args):
         with open(".gitignore", "a+") as f:
             f.readlines
             f.writelines([
-            "" if f.tell() == 0 else "\n",
-            "# TypeO\n",
-            "dependency_override\n",
-            "*.sln\n",
-            "*.csproj"
-        ])
+                "" if f.tell() == 0 else "\n",
+                "# TypeO\n",
+                "dependency_override\n",
+                "*.sln\n",
+                "*.csproj"
+            ])
 
-    CreateFile("dependency_override", "[\n]")
+    CreateFileFromTemplate("dependency_override", "templates/dependency_override.template", {})
     
     dependencies = args.dependencies.split()
     if len(dependencies) == 0:
@@ -61,120 +61,80 @@ def addExtraFiles(args):
     }
     CreateFile("product", json.dumps(product, ensure_ascii=False, indent=4))
 
-    CreateFile("Readme-TypeO.txt",
-"""Please distribute this file with the TypeO runtime environment:
+    CreateFileFromTemplate("Readme-TypeO.txt", "templates/Readme-TypeO.template", {})
 
-TypeO is available from:
-http://typeo.typedeaf.com/
-
-This library is distributed under the terms of the zlib license:
-http://www.zlib.net/zlib_license.html""")
-
-    CreateFile("ReleaseNotes-{}.txt".format(args.name),
-"""*******************************
-******** RELEASE NOTES ********
-*******************************
-
-**************************
-**** {} - {}/{}-{} ****
-**************************
-
-******************
-**** Features ****
-******************
-
-
-******************
-***** Fixes ******
-******************
-
-
-******************
-** Improvements **
-******************
-
-""".format(args.version, date.today().day, date.today().month, str(date.today().year)[0:2]))
+    CreateFileFromTemplate("ReleaseNotes-{}.txt".format(args.name), "templates/ReleaseNotes.template", {
+        "version": args.version,
+        "day": date.today().day,
+        "month": date.today().month,
+        "year": str(date.today().year)[0:2]
+    })
     
     batLine = "py typer/typer.py generate -p ."
     if args.xUnit:
         batLine = "{} --xUnit".format(batLine)
     if args.dev:
         batLine = "{} --dev".format(batLine)
-    CreateFile("create_project_files.bat",
-"""@ECHO OFF
-{}""".format(batLine))
+    CreateFileFromTemplate("create_project_files.bat", "templates/create_project_files.template", {
+        "command": batLine
+    })
 
 def createCodeFiles(args):
-    CreateFile("{}/{}Module.cs".format(args.name, args.name),
-"""using TypeOEngine.Typedeaf.Core.Engine;
-namespace {};
+    CreateFileFromTemplate("{}/{}Module.cs".format(args.name, args.name), "templates/code/Module.template", {
+        "name": args.name
+    })
 
-/// <summary>
-/// </summary>
-public class {}Module : Module<{}ModuleOption>
-{{
-    /// <inheritdoc/>
-    protected override void Initialize() {{ }}
-
-    /// <inheritdoc/>
-    protected override void Cleanup() {{ }}
-
-    /// <inheritdoc/>
-    protected override void LoadExtensions(TypeO typeO) {{ }}
-}}""".format(args.name, args.name, args.name))
-
-    CreateFile("{}/{}ModuleOption.cs".format(args.name, args.name),
-"""using TypeOEngine.Typedeaf.Core.Engine;
-namespace {};
-
-/// <summary>
-/// </summary>
-public class {}ModuleOption : ModuleOption
-{{
-}}""".format(args.name, args.name))
+    CreateFileFromTemplate("{}/{}ModuleOption.cs".format(args.name, args.name), "templates/code/ModuleOption.template", {
+        "name": args.name
+    })
 
 def createCodeTestFiles(args):
     testName = "{}Test".format(args.name)
-    CreateFile("{}/{}ModuleTest.cs".format(testName, args.name),
-"""using System.Linq;
-using TypeOEngine.Typedeaf.Core;
-using TypeOEngine.Typedeaf.Core.Engine;
-using {};
+    CreateFileFromTemplate("{}/{}ModuleTest.cs".format(testName, args.name), "templates/codetest/ModuleTest.template", {
+        "name":     args.name,
+        "testName": testName
+    })
 
-namespace {};
+    CreateFileFromTemplate("{}/Usings.cs".format(testName), "templates/codetest/Usings.template", {})
 
-public class TestGame : Game
-{{
-    public static string GameName {{ get; set; }} = "test";
-    public override void Initialize() {{}}
-    public override void Update(double dt) {{ Exit(); }}
-    public override void Draw() {{ }}
-    public override void Cleanup() {{ }}
-}}
+def createCodeTypeDTestFiles(args):
+    testName = "{}Test".format(args.name)
+    typeDName = "TypeD{}".format(args.name)
 
-public class {}
-{{
-    [Fact]
-    public void StartTest()
-    {{
-        var typeO = TypeO.Create<TestGame>(TestGame.GameName)
-                .LoadModule<{}Module>() as TypeO;
-        Assert.NotNull(typeO);
-        typeO.Start();
-        var module = typeO.Context.Modules.FirstOrDefault(m => m.GetType() == typeof({}Module)) as {}Module;
-        Assert.NotNull(module);
-        Assert.IsType<{}Module>(module);
-        Assert.NotEmpty(typeO.Context.Modules);
-    }}
-}}""".format(args.name, testName, testName, args.name, args.name, args.name, args.name))
+    CreateFileFromTemplate("{}/TypeDMock.cs".format(testName), "templates/codetest/TypeDMock.template", {
+        "testName": testName
+    })
 
-    CreateFile("{}/Usings.cs".format(testName), "global using Xunit;")
+    CreateFileFromTemplate("{}/{}ModuleTest.cs".format(testName, typeDName), "templates/codetest/TypeDModuleTest.template", {
+        "testName": testName,
+        "typeDName": typeDName
+    })
+
+def createCodeTypeDFiles(args):
+    typeDName = "TypeD{}".format(args.name)
+    CreateFileFromTemplate("{}/{}Initializer.cs".format(typeDName, typeDName), "templates/codetyped/Initializer.template", {
+        "typeDName": typeDName
+    })
 
 def CreateFile(file, content):
     if not os.path.exists(file):
-        print("Creating {}".format(file))
+        print("Creating '{}'".format(file))
         with open(file, "w") as f:
             f.write(content)
+
+def CreateFileFromTemplate(file, templateFile, content):
+    templateFile = "{}/{}".format(os.path.dirname(os.path.realpath(__file__)), templateFile)
+    if not os.path.exists(templateFile):
+        print("Can't find template '{}'".format(templateFile))
+        return
+    if not os.path.exists(file):
+        print("Creating '{}' from template '{}'".format(file, templateFile))
+        with open(templateFile, "r") as t:
+            template = t.read()
+            for key in content:
+                template = template.replace("{{{{{}}}}}".format(key), str(content[key]))
+            with open(file, "w") as f:
+                f.write(template)
 
 def do(args):
     projectPath = "{}/{}".format(args.output, args.name.lower())
@@ -189,8 +149,12 @@ def do(args):
 
     if not args.clean:
         createCodeFiles(args)
+        if args.dev:
+            createCodeTypeDFiles(args)
         if args.xUnit:
             createCodeTestFiles(args)
+            if args.dev:
+                createCodeTypeDTestFiles(args)
 
     if not args.disableGit:
         os.system("git add .")
---
+++ scripts/templates/Readme-TypeO.template
@@ -0,0 +1,7 @@
+Please distribute this file with the TypeO runtime environment:
+
+TypeO is available from:
+http://typeo.typedeaf.com/
+
+This library is distributed under the terms of the zlib license:
+http://www.zlib.net/zlib_license.html
